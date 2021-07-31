bfe76d8988b12f424fa3f9787beaed86eb3860a9
Steve Halliwell <halliwell.steve@gmail.com>
2021-07-31 18:34:14 +1000

    Add DiLibrary and tests

---
+++ Assets/ulox/Runtime/Library/DiLibrary.cs
@@ -0,0 +1,36 @@
+﻿namespace ULox
+{
+    public class DiLibrary : IULoxLibrary
+    {
+        private DiContainer _diCont;
+
+        public DiLibrary(DiContainer cont)
+        {
+            _diCont = cont;
+        }
+
+        public string Name => nameof(DiLibrary);
+
+        public Table GetBindings()
+        {
+            var resTable = new Table();
+            var assertInst = new InstanceInternal();
+            resTable.Add("DI", Value.New(assertInst));
+
+            assertInst.fields[nameof(Count)] = Value.New(Count);
+            assertInst.fields[nameof(GenerateDump)] = Value.New(GenerateDump);
+
+            return resTable;
+        }
+
+        private Value Count(VMBase vm, int argCount)
+        {
+            return Value.New(_diCont.Count);
+        }
+
+        private Value GenerateDump(VMBase vm, int argCount)
+        {
+            return Value.New(_diCont.GenerateDump());
+        }
+    }
+}
---
+++ Assets/ulox/Runtime/VM/DiContainer.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace ULox
+{
+    public class DiContainer : Table
+    {
+        internal DiContainer ShallowCopy()
+        {
+            var ret = new DiContainer();
+            foreach (var pair in this)
+            {
+                ret.Add(pair.Key, pair.Value);
+            }
+            return ret;
+        }
+
+        internal void ReplaceWith(DiContainer diContainerToRestore)
+        {
+            Clear(); 
+            foreach (var pair in diContainerToRestore)
+            {
+                Add(pair.Key, pair.Value);
+            }
+        }
+
+        internal string GenerateDump()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("Registered in DI:");
+
+            foreach (var item in this)
+            {
+                sb.AppendLine($"{item.Key}:{item.Value}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}

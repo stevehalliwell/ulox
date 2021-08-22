08b7468ca5b15c42e17f363e8bc6c71928a69122
Steve Halliwell <halliwell.steve@gmail.com>
2021-08-22 20:16:16 +1000

    Add Duplicate to CoreLibrary
    Does a shallow copy

--- Assets/ulox/Runtime/DataTypes/Table.cs
+++ Assets/ulox/Runtime/DataTypes/Table.cs
@@ -4,6 +4,14 @@ namespace ULox
 {
     public class Table : Dictionary<string, Value>
     {
+        public Table()
+        {
+        }
+
+        public Table(IDictionary<string, Value> dictionary) : base(dictionary)
+        {
+        }
+
         public static Table Empty() => new Table();
     }
 }
--- Assets/ulox/Runtime/Library/CoreLibrary.cs
+++ Assets/ulox/Runtime/Library/CoreLibrary.cs
@@ -13,14 +13,21 @@
         {
             var resTable = new Table();
 
-            resTable.Add("print", Value.New(
-                (vm, args) =>
-                {
-                    _printer?.Invoke(vm.GetArg(1).ToString());
-                    return Value.Null();
-                }));
+            resTable.Add(nameof(print), Value.New(print));
+            resTable.Add(nameof(Duplicate), Value.New(Duplicate));
 
             return resTable;
+        }
+
+        public Value print(VMBase vm, int argCount)
+        {
+            _printer?.Invoke(vm.GetArg(1).ToString());
+            return Value.Null();
+        }
+
+        public Value Duplicate(VMBase vm, int argCount)
+        {
+            return Value.Copy(vm.GetArg(1));
         }
     }
 }

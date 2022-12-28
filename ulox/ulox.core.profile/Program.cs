6b2e9b9528dacf2f656dda78bf58f2ee4f62a34b
Steve Halliwell <halliwell.steve@gmail.com>
2022-12-28 20:20:11 +1000

    Add project for visual studio analysis profiling

---
+++ ulox/ulox.core.profile/Program.cs
@@ -0,0 +1,134 @@
+﻿using ULox;
+
+var compileRepeat = 1000;
+var runRepeat = 1000;
+var callFuncRepeat = 1000;
+var funcSetupName = "SetupGame";
+var funcUpdateName = "Update";
+
+var targetScriptText = @"
+/*
+Code we expect from Unity's side;
+SetUIText(string)
+CreateFromPrefab(string):GameObject (returns a GameObject reference that we'll pass back to unity)
+GetKey(string):bool
+SetGameObjectPosition(GameObject, x, y, z)
+ReloadScene - reload the current unity scene
+
+
+Unity will find and call: 
+- SetupGame in Start
+- Update in Update
+*/
+
+//functions and data
+var dt = 0;
+var limit = 5;
+var numBallsToSpawn = 200;
+
+var ballName = ""BouncyBall"";
+var balls = [];
+
+fun RandVec2()
+{
+	return (-3,3);
+}
+
+class Ball
+{
+	init()
+	{
+		var (x,y) = RandVec2();
+		this.posx = x;
+		this.posy = y;
+
+		var (vx,vy) = RandVec2();
+		this.velx = vx;
+		this.vely = vy;
+		
+		//this.go = CreateFromPrefab(ballName);
+	}
+
+	Tick()
+	{
+  		this.posx = this.posx + this.velx * dt;
+  		this.posy = this.posy + this.vely * dt;
+
+		this.Contain(limit);
+		//this.UpdateGoPosition();
+	}
+	
+	Contain(lim)
+	{
+		var x = this.posx;
+		var y = this.posy;
+		var vx = this.velx;
+		var vy = this.vely;
+
+		if((x < -lim and vx < 0) or 
+		  (x > lim  and vx > 0) )
+		{
+			vx *= -1;
+			this.velx = vx;
+		}
+
+		if((y < -lim and vy < 0) or
+		  (y > lim  and vy > 0) )
+		{
+			vy *= -1;
+			this.vely = vy;
+		}
+	}
+}
+
+fun SetupGame()
+{
+	print (""Setting Up Game"");
+
+	for(var i = 0; i < numBallsToSpawn; i += 1)
+	{
+		balls.Add(Ball());
+	}
+}
+
+fun Update()
+
+	loop(balls)
+	{
+		item.Tick();
+	}
+}
+
+";
+
+var targetScript = new Script("", targetScriptText);
+
+while (true)
+{
+    Console.WriteLine($"Compiling only {compileRepeat} times.");
+    for (int i = 0; i < compileRepeat; i++)
+    {
+        var engine = Engine.CreateDefault();
+        var compiled = engine.Context.CompileScript(targetScript);
+    }
+
+    Console.WriteLine($"RunScript {runRepeat} times.");
+    for (int i = 0; i < runRepeat; i++)
+    {
+        var engine = Engine.CreateDefault();
+        engine.RunScript(targetScript);
+    }
+
+    Console.WriteLine($"Call {funcUpdateName} {callFuncRepeat} times.");
+    {
+        var engine = Engine.CreateDefault();
+        engine.RunScript(targetScript);
+        var funcStartValue = engine.Context.Vm.GetGlobal(new HashedString(funcSetupName));
+        var funcUpdateValue = engine.Context.Vm.GetGlobal(new HashedString(funcUpdateName));
+        engine.Context.Vm.PushCallFrameAndRun(funcStartValue, 0);
+        for (int i = 0; i < callFuncRepeat; i++)
+        {
+            engine.Context.Vm.PushCallFrameAndRun(funcUpdateValue, 0);
+        }
+    }
+}

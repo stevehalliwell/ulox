747e769881b6f65dd2ec8cf7909b74c5671325a7
Steve Halliwell <halliwell.steve@gmail.com>
2023-12-18 19:05:09 +1000

    Prove we can do desugar string interp
    Horrible Hack

--- ulox/ulox.core.tests/ScannerTests.cs
+++ ulox/ulox.core.tests/ScannerTests.cs
@@ -310,31 +310,6 @@ fun foo(p)
         }
 
         [Test]
-        public void StringInterp_WhenUsed_ShouldMatchTokens()
-        {
-            var tokenResults = new TokenType[]
-            {
-                TokenType.VAR,
-                TokenType.IDENTIFIER,
-                TokenType.ASSIGN,
-                TokenType.STRING,
-                TokenType.PLUS,
-                TokenType.OPEN_PAREN,
-                TokenType.NUMBER,
-                TokenType.CLOSE_PAREN,
-                TokenType.PLUS,
-                TokenType.STRING,
-                TokenType.END_STATEMENT,
-                TokenType.EOF
-            };
-            var testString = @"var s = ""Hi, {3}"";";
-
-            var resultingTokenTypes = scanner.Scan(new Script("test", testString));
-
-            CollectionAssert.AreEqual(tokenResults, resultingTokenTypes.Select(x => x.TokenType).ToArray());
-        }
-
-        [Test]
         public void StringInterp_WhenEscaped_ShouldMatchTokens()
         {
             var tokenResults = new TokenType[]
@@ -351,6 +326,69 @@ fun foo(p)
             var resultingTokenTypes = scanner.Scan(new Script("test", testString));
 
             CollectionAssert.AreEqual(tokenResults, resultingTokenTypes.Select(x => x.TokenType).ToArray());
+        }
+    }
+
+    public class DesugarTests
+    {
+        [Test]
+        public void Empty_WhenDesugar_DoesNotThrow()
+        {
+            var scriptContent = @"";
+            var (_, tokenIterator) = Prepare(scriptContent);
+            var res = new List<Token>();
+
+            AdvanceGather(tokenIterator, res);
+
+            Assert.AreEqual(1, res.Count);
+        }
+
+        [Test]
+        public void DeclareVAr_WhenDesugar_ShouldHaveSame()
+        {
+            var scriptContent = @"var s = 1;";
+            var (tokens, tokenIterator) = Prepare(scriptContent);
+            var startingCount = tokens.Count;
+            var res = new List<Token>();
+
+            AdvanceGather(tokenIterator, res);
+
+            Assert.AreEqual(res.Count, startingCount);
+        }
+
+        [Test]
+        public void StringInterp_WhenDesugar_ShouldHaveMoreTokens()
+        {
+            var scriptContent = @"var s = ""Hi, {3}"";";
+            var (tokens, tokenIterator) = Prepare(scriptContent);
+            var startingCount = tokens.Count;
+            var res = new List<Token>();
+
+            AdvanceGather(tokenIterator, res);
+
+            Assert.Greater(res.Count, startingCount);
+        }
+
+        private static (List<Token> tokens, TokenIterator tokenIterator) Prepare(string scriptContent)
+        {
+            var scanner = new Scanner();
+            var script = new Script("test", scriptContent);
+            var tokens = scanner.Scan(script);
+            var tokenIterator = new TokenIterator(script, tokens);
+            return (tokens, tokenIterator);
+        }
+
+        private static void AdvanceGather(TokenIterator tokenIterator, List<Token> list)
+        {
+            tokenIterator.Advance();
+
+            while (tokenIterator.CurrentToken.TokenType != TokenType.EOF)
+            {
+                list.Add(tokenIterator.CurrentToken);
+                tokenIterator.Advance();
+            }
+
+            list.Add(tokenIterator.CurrentToken);
         }
     }
 }
--- ulox/ulox.core/Package/Runtime/Compiler/TokenIterator.cs
+++ ulox/ulox.core/Package/Runtime/Compiler/TokenIterator.cs
@@ -10,7 +10,7 @@ namespace ULox
 
         private readonly Script _script;
         private readonly List<Token> _tokens;
-        
+
         private int _currentTokenIndex = -1;
 
         public TokenIterator(Script script, List<Token> tokens)
@@ -28,6 +28,68 @@ namespace ULox
         {
             PreviousToken = CurrentToken;
             CurrentToken = _tokens[++_currentTokenIndex];
+            if (CurrentToken.TokenType == TokenType.STRING)
+            {
+                var literalString = CurrentToken.Literal as string;
+                var newStr = literalString;
+                var locStart = InterpolationStart(literalString, 0);
+                if (locStart != -1)
+                {
+                    var locEnd = InterpolationEnd(literalString, locStart + 1);
+                    newStr = literalString.Substring(0, locStart);
+
+                    var interpStr = literalString.Substring(locStart + 1, locEnd - locStart - 1);
+                    var postInterpStr = literalString.Substring(locEnd + 1);
+                    var scanner = new Scanner();
+                    var interpTokens = scanner.Scan(new Script(_script.Name + "_interp", interpStr));
+                    interpTokens.RemoveAt(interpTokens.Count-1);
+                    interpTokens.Insert(0, new Token(
+                        TokenType.OPEN_PAREN,
+                        "(",
+                        null,
+                        CurrentToken.Line,
+                        CurrentToken.Character, //tmp
+                        CurrentToken.StringSourceIndex));//tmp
+                    interpTokens.Insert(0, new Token(
+                        TokenType.PLUS,
+                        "+",
+                        null,
+                        CurrentToken.Line,
+                        CurrentToken.Character, //tmp
+                        CurrentToken.StringSourceIndex));//tmp
+                    interpTokens.Add(new Token(
+                       TokenType.CLOSE_PAREN,
+                       ")",
+                       null,
+                       CurrentToken.Line,
+                       CurrentToken.Character, //tmp
+                       CurrentToken.StringSourceIndex));//tmp
+                    interpTokens.Add(new Token(
+                       TokenType.PLUS,
+                       "+",
+                       null,
+                       CurrentToken.Line,
+                       CurrentToken.Character, //tmp
+                       CurrentToken.StringSourceIndex));//tmp
+                    interpTokens.Add(new Token(
+                       TokenType.STRING,
+                       postInterpStr,
+                       postInterpStr,
+                       CurrentToken.Line,
+                       CurrentToken.Character, //tmp
+                       CurrentToken.StringSourceIndex));//tmp
+                    _tokens.InsertRange(_currentTokenIndex + 1, interpTokens);
+                }
+
+                newStr = System.Text.RegularExpressions.Regex.Unescape(newStr);
+                CurrentToken = new Token(
+                    TokenType.STRING,
+                    newStr,
+                    newStr,
+                    CurrentToken.Line,
+                    CurrentToken.Character,
+                    CurrentToken.StringSourceIndex);
+            }
         }
 
         public void Consume(TokenType tokenType, string msg)
@@ -59,6 +121,43 @@ namespace ULox
                 return true;
             }
             return false;
+        }
+
+        private static int InterpolationStart(string literalString, int startAt)
+        {
+            var loc = literalString.IndexOf('{', startAt);
+            if (loc == -1)
+                return -1;
+
+            if (loc == 0)
+                return loc;
+
+            var prevChar = literalString[loc - 1];
+            if (prevChar == '\\')
+                return InterpolationStart(literalString, loc + 1);
+
+            return loc;
+        }
+
+        private static int InterpolationEnd(string literalString, int loc)
+        {
+            var end = literalString.Length;
+            var requiredClose = 1;
+            while (loc < end)
+            {
+                var ch = literalString[loc];
+                if (ch == '{')
+                    requiredClose++;
+                else if (ch == '}')
+                    requiredClose--;
+                
+                if (requiredClose <= 0)
+                    return loc;
+                
+                loc++;
+            }
+
+            return loc;
         }
     }
 }

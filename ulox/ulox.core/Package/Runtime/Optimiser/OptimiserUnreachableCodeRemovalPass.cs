c3e6934809a1b1319fb180c07c27f6a12fd4449f
Steve Halliwell <halliwell.steve@gmail.com>
2024-09-19 14:43:37 +1000

    Improve performance of OptimiserUnreachableCodeRemovalPass

--- ulox/ulox.core/Package/Runtime/Optimiser/OptimiserUnreachableCodeRemovalPass.cs
+++ ulox/ulox.core/Package/Runtime/Optimiser/OptimiserUnreachableCodeRemovalPass.cs
@@ -7,10 +7,12 @@ namespace ULox
     public sealed class OptimiserUnreachableCodeRemovalPass : IOptimiserPass
     {
         private readonly List<int> _unreachablePoint = new();
+        private List<int> _allLabelEnds;
 
         public void Prepare(Optimiser optimiser, Chunk chunk)
         {
             _unreachablePoint.Clear();
+            _allLabelEnds = chunk.Labels.Values.OrderBy(x=>x).ToList();
         }
 
         public void ProcessPacket(Optimiser optimiser, Chunk chunk, int inst, ByteCodePacket packet)
@@ -19,7 +21,7 @@ namespace ULox
             {
             case OpCode.GOTO:
             case OpCode.RETURN:
-                if (!chunk.Labels.Any(x => x.Value == inst))
+                if (_allLabelEnds.BinarySearch(inst) < 0)//bsearch here gives lower bound compliment on fail
                     _unreachablePoint.Add(inst + 1);
                 break;
             }
